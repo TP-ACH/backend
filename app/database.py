4f31beaf4e7d5d529f4183fb3479cfaa1b9f9ce5
franjday <fday@fi.uba.ar>
2024-07-01 18:46:14 -0300

    add endpoint for data fetch

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+from typing import Dict
 from motor import motor_asyncio
 from logger import logger
 
@@ -14,7 +15,6 @@ MONGODB_PASSWORD = os.getenv("MONGO_INITDB_PASSWORD")
 MONGODB_URI = f"mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}:{MONGODB_PORT}/{MONGODB_DB}?retryWrites=true&w=majority"
 
 mongo_client = motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
-db = mongo_client.get_database(MONGODB_DB)
 
 async def validate_connection():
     try:
@@ -24,12 +24,25 @@ async def validate_connection():
         logger.error(f"Failed to connect to MongoDB: {e}")
 
 
-def insert_data(db, collection, reading):
-    db = mongo_client.get_database(db)
+def insert_data(db_name, collection, reading):
+    db = mongo_client.get_database(db_name)
     data_collection = db.get_collection(collection)
     data_entry = {
-        "device_id": 32,
         "reading": reading,
         "created_at": datetime.datetime.now()
     }
     data_collection.insert_one(data_entry)
+
+async def fetch_data(device_id: str, query: Dict):
+    db = mongo_client.get_database(device_id)
+    collection_names = await db.list_collection_names()
+    
+    all_data = {}
+    
+    for collection_name in collection_names:
+        collection = db.get_collection(collection_name)
+        projection = {"reading": 1, "created_at": 1, "_id": 0}
+        cursor = collection.find(query, projection)
+        all_data[collection_name] = await cursor.to_list(length=None)
+    
+    return all_data
