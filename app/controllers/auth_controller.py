6c0504e3c66d65b3f8f825bbabe1f8e8b4820423
Juan Bautista Rodriguez <jbrodriguez@fi.uba.ar>
2024-08-24 14:42:56 -0300

    not hardcoding redirect uri

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -8,29 +8,23 @@ import httpx
 router = APIRouter()
 
 @router.get("/login")
-async def login():
-    auth_req = await get_login_request("http://localhost:8000/auth/callback")
-    logger.info(auth_req)
+async def login(request: Request):
+    url = request.url_for("auth_callback")
+    auth_req = await get_login_request(url)
     return RedirectResponse(auth_req)
 
 @router.get("/callback")
 async def auth_callback(request: Request):
+    logger.info(str(request.url))
     code = request.query_params.get("code")
     if not code:
         return JSONResponse(status_code=400, content={"message": "Authorization code not provided"})
-
-    req = await get_token_request("http://localhost:8000/auth/callback", code)
+    url = request.url_for("auth_callback")
+    req = await get_token_request(url, code)
 
     async with httpx.AsyncClient() as client:
-        # response = await client.post(req["url"], **req["kwargs"])
-        headers=req["kwargs"]["headers"]
-        data=req["kwargs"]["data"]
-        logger.info("********** POST ***********")
-        logger.info(headers)
-        logger.info(data)
-        logger.info(req["url"])
-        response = await client.post(req["url"], headers=headers, data=data)
-
+        response = await client.post(req["url"], **req["kwargs"])
+        
         if response.status_code != 200:
             return JSONResponse(status_code=response.status_code, content={"message": response.text})
 
