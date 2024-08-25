835310c5991b6cf5e67323eff9925f86cebfe24a
bauti <jbrodriguez@fi.uba.ar>
2024-08-25 13:42:35 -0300

    move business logic to ha_client

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,7 +1,7 @@
 from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi import APIRouter, Request
 from utils.logger import logger
-from clients.homeassistant_client import get_login_request, get_token_request
+from clients.homeassistant_client import get_login_request, get_token_request, get_access_token_response
 from models.template import Attribute
 import httpx
 
@@ -21,15 +21,12 @@ async def auth_callback(request: Request):
         return JSONResponse(status_code=400, content={"message": "Authorization code not provided"})
     redirect_uri = request.url_for("auth_callback")
     client_id = str(request.url).rsplit("/", 2)[0]
-    req = await get_token_request(redirect_uri, client_id, code)
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(req["url"], **req["kwargs"])
-        
-        if response.status_code != 200:
-            return JSONResponse(status_code=response.status_code, content={"message": response.text})
-
-        access_token = response.json()['access_token']
-        logger.info("Access token retrieved successfully")
-        return JSONResponse(status_code=200, content={"access_token": access_token})
     
+    response = await get_access_token_response(redirect_uri, client_id, code)
+
+    if response.status_code != 200:
+        return JSONResponse(status_code=response.status_code, content={"message": response.text})
+
+    access_token = response.json()['access_token']
+    logger.info("Access token retrieved successfully")
+    return JSONResponse(status_code=200, content={"access_token": access_token})
