b0547671cf50e591ed75e1fbe5f9a1b39c6ccbda
franjday <fday@fi.uba.ar>
2024-10-31 18:48:50 -0300

    remove celery and redis
    * black reformat
    
    * remove celery and redis

--- app/celery_app.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-celery_app = Celery(
-    "celery_app",
-    broker=os.getenv("CELERY_BROKER_URL"),
-    backend=os.getenv("CELERY_RESULT_BACKEND"),
-    include=["tasks"],
-)
